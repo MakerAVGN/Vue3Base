2501e42cfa7880f3f9b660287dedb1062190e45f
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-06 22:03:59 +0300

    Навешивание обработчиков событий в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,15 +5,16 @@ export default {
   },
   methods: {
     show: function () {
-        const current = new Date();
-        const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
-        alert(date);
+      const current = new Date();
+      const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+      alert(date);
     }
   }
 }
 </script>
 <template>
-<h1>№1 {{ show() }}</h1> <br>
+<h1>№2</h1> <br>
+<button @mouseenter="show">Date</button>
 
 
 </template>
