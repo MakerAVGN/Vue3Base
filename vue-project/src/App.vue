1ee30c0d97d04de5ded8bed3fbfa1ea3e108feba
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-06 20:42:11 +0300

    Объект дата в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,48 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      firstname: 'Eren',
+      secondname: 'Eger',
+    }
+  }
+}
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="My first vue project!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-  <h2>Added text</h2>
+<h1>
+  {{ firstname }}
+</h1>
+<h2>
+{{ secondname }}
+</h2>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
