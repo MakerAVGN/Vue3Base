f95f2011c9ff0dd3db5eef932949c04bea019505
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-06 21:16:08 +0300

    Массивы в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,16 +2,19 @@
 export default {
   data() {
     return {
-      num0:5,
-      num1:1,
-      num2:2,
-      nim3:3,
+      arr: ['x', 'y', 'z'],
+      arr: [1, 2, 3],
     }
   }
 }
 </script>
 <template>
-<p>Квадрат свойства num0: {{ num0 * num0 }} </p> <br>
-<p>Сумма свойства num1-3: {{ num1 + num2 + num3 }}</p>
+<p>№1</p> <br>
+<h1>{{ arr[0] }}</h1> <br>
+<h2>{{ arr[1] }}</h2><br>
+<h3>{{ arr[2] }}</h3> <br> <br>
+<p>№2</p> <br>
+
+<p>{{ arr[0] + arr[1] + arr[2] }}</p>
 
 </template>
