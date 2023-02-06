455b319add45bcb560d102dcfc33f6e0a0e063dc
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-06 21:18:28 +0300

    Объекты в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,19 +2,13 @@
 export default {
   data() {
     return {
-      arr: ['x', 'y', 'z'],
-      arr: [1, 2, 3],
+      obj: { x: 1, y: 2, z: 3 },
     }
   }
 }
 </script>
 <template>
-<p>№1</p> <br>
-<h1>{{ arr[0] }}</h1> <br>
-<h2>{{ arr[1] }}</h2><br>
-<h3>{{ arr[2] }}</h3> <br> <br>
-<p>№2</p> <br>
-
-<p>{{ arr[0] + arr[1] + arr[2] }}</p>
+<h1>№1</h1> <br>
+<h2>{{ obj['x'] + obj['y'] + obj['z'] }}</h2>
 
 </template>
