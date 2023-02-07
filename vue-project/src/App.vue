9b663d0a10a9c7180304bab592b76c2ad702c57b
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 14:23:29 +0300

    Вспомогательные методы в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -9,7 +9,36 @@ export default {
   },
   methods: {
     show: function () {
-      alert(this.num1 + this.num2);
+      const current = new Date();
+      const dayOfWeekNumber = current.getDay();
+      let dayOfWeek;
+      switch (dayOfWeekNumber) {
+        case 0:
+          dayOfWeek = "Sunday";
+          break;
+        case 1:
+          dayOfWeek = "Monday";
+          break;
+        case 2:
+          dayOfWeek = "Tuesday";
+          break;
+        case 3:
+          dayOfWeek = "Wednesday";
+          break;
+        case 4:
+          dayOfWeek = "Thursday";
+          break;
+        case 5:
+          dayOfWeek = "Friday";
+          break;
+        case 6:
+          dayOfWeek = "Saturday";
+          break;
+        default:
+          dayOfWeek = "Something went wrong"
+          break;
+      }
+      alert(`Day of week by number = ${dayOfWeekNumber}. Day of week = ${dayOfWeek}`);
     }
   }
 }
