52e6bae712f0aa09a12de3248a7f34a11ace752f
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 15:21:47 +0300

    Передача параметров в метод в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -8,44 +8,31 @@ export default {
     }
   },
   methods: {
-    show: function () {
-      const current = new Date();
-      const dayOfWeekNumber = current.getDay();
-      let dayOfWeek;
-      switch (dayOfWeekNumber) {
-        case 0:
-          dayOfWeek = "Sunday";
-          break;
-        case 1:
-          dayOfWeek = "Monday";
-          break;
-        case 2:
-          dayOfWeek = "Tuesday";
-          break;
-        case 3:
-          dayOfWeek = "Wednesday";
-          break;
-        case 4:
-          dayOfWeek = "Thursday";
-          break;
-        case 5:
-          dayOfWeek = "Friday";
-          break;
-        case 6:
-          dayOfWeek = "Saturday";
-          break;
-        default:
-          dayOfWeek = "Something went wrong"
-          break;
-      }
-      alert(`Day of week by number = ${dayOfWeekNumber}. Day of week = ${dayOfWeek}`);
+    summ: function () {
+      const number = this.$refs.inputmult.value;
+      alert(number * number);
+    },
+
+    mult_two: function() {
+      alert(2 * 2);
+    },
+    mult_three: function () {
+      alert(3 * 3);
     }
   }
+  
 }
+
+
 </script>
 <template>
-<h1>№2</h1> <br>
-<button @click="show">Date</button>
+<h1>№1</h1> <br>
+<input type="number" ref="inputmult"><br>
+<button @click="summ">Click</button>
+<br>
+<br>
 
 
+<button @click="mult_two">Square number 2</button> <br>
+<button @click="mult_three">Square number 3</button>
 </template>
