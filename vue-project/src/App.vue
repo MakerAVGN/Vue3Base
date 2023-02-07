0938a4a4bde368314123f44d40d973b871595703
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-07 16:08:06 +0300

    Показ по условию в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,18 +2,17 @@
 export default {
   data() {
     return {
-      text:'Модификаторы событий в фреймворке Vue',
-      url: 'https://v3.ru.vuejs.org/ru/guide/events.html#модификаторы-событии'
+      visible: true,
+      not_visible: false,
     }
   },
   methods: {
-    warning: function(){
-      alert('JUST ONE TIME!');
-    }
+    
+    
   }
 }
 </script>
 <template>
-  <a @click.prevent :href=url>{{ text }}</a> <br>
-  <button @click.once="warning"> Just one time </button>
+  <p v-if="visible" > Видишь меня? </p>
+  <p v-if="not_visible" > А меня нет))) </p>
 </template>
