1195c482a2084c2ec17fd0734bdf1f12178668d2
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-22 17:03:52 +0300

    Перебор объектов в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,51 +1,37 @@
-<style>
-*{
-  background-color: #3d405b;
-}
-div {
-  display: flex;
-  font-size: 1.2rem;
-}
-.a{
-  border: 3px solid #e07a5f;
-  margin: 5px;
-  padding: 10px;
-  align-items: center;
-  flex-direction: column;
-  color:#e07a5f ;
-}
-.b{
-  border: 3px solid #f4f1de;
-  flex-direction: column;
-  text-align: center;
-  margin: 5px;
-  padding: 10px;
-  color: #f4f1de;
-}
-</style>
 <script>
 export default {
   data() {
     return {
-        arr: ['x', 'y', 'z'],
+      data: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$',
+      }
     }
   },
   methods: {
-    toggle: function () {
-      this.isAuth = !this.isAuth;
-    }
-    
   }
 }
 </script>
 <template>
- <p class="b" v-for="(elem, key) in arr">
-  		 <ul><li>{{ key + 1 }} {{ elem }}</li></ul>
-  	</p>
+  	<div class="a">
+      <p v-for="elem in data">
+      {{ elem }}; </p>
+    </div>
 
-  <p class="a" v-for="(elem, key) in arr">
-    		 <ul><li>{{ key + 1 }} {{ elem }}</li></ul>
-    	</p>
+    <div class="b">
+      <p v-for="(elem,key) in data" >
+      {{ key }} - {{ elem }}</p>
+    </div>
+
+    <div class="b">
+      <p v-for="(elem,key, index) in data" >
+      {{ key }} - {{ elem }} - {{ index }}</p>
+    </div>
+    <div class="a">
+        <p v-for="(elem, key, index) in data" >
+        {{ key }} - {{ elem }} - {{ index + 1 }}</p>
+      </div>
 </template>
 
 
