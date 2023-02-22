43a81b1689bc0e351f5b154ee059f5297353aec7
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-22 17:55:01 +0300

    Атрибут key в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,22 +1,31 @@
 <script>
 export default {
-  data() {
+ data() {
     return {
-      items: [1, -2, 3, -4, 5],
+      products: [
+        {
+          id: 1,
+          name: 'product1',
+        },
+        {
+          id: 2,
+          name: 'product2',
+        },
+        {
+          id: 3,
+          name: 'product3',
+        },
+      ]
     }
-  },
-  methods: {
   }
 }
 </script>
 <template>
-	<ul>
-		<template v-for="elem in items">
-			<li v-if="elem > 0">
-				{{ elem }}
-			</li>
-		</template>
-	</ul>
+<ul>
+    <li v-for="user in products" :key="user.id">
+      {{ user.name }}
+    </li>
+  </ul>
 </template>
 
 
