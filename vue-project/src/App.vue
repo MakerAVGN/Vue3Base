eae544c9df02880a1024c7a2de9bc6f2e685a544
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-22 17:39:01 +0300

    Перебор массива объектов в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,9 +2,28 @@
 export default {
   data() {
     return {
-      
-        items: [1, 2, 3],
-      
+      hrefs: [
+        { href: '1.html', text: 'text1' },
+        { href: '2.html', text: 'text2' },
+        { href: '3.html', text: 'text3' },
+      ],
+      products: [
+        {
+          name: 'product1',
+          price: 100,
+          quantity: 5
+        },
+        {
+          name: 'product2',
+          price: 200,
+          quantity: 4
+        },
+        {
+          name: 'product3',
+          price: 300,
+          quantity: 3
+        },
+      ]
     }
   },
   methods: {
@@ -12,17 +31,27 @@ export default {
 }
 </script>
 <template>
- <template v-for="elem in arr">
-		<p>{{ elem }}</p>
-		<hr>
-	</template>
-  	
-    <li class="a" v-for="elem in items">
-        <ul>
-         {{ elem }}
-        </ul>
+ <div class="a">
+     <ul>
+      <li v-for="href in hrefs">
+        {{ href.href }}
+        {{ href.text }}
       </li>
-      <br>
+     </ul>
+ </div>
+
+<div class="b">
+  <tr>
+    <td v-for="text in products">
+      {{ text.name }} <br>
+      {{ text.price }} <br>
+      {{ text.quantity }}
+    </td>
+  </tr>
+</div>
+
+
+
 
 </template>
 
