12a50bbb2c97223196f1bc05bc90a6bc6c1fc6fd
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-22 18:12:26 +0300

    Атрибут class в фреймворке Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,36 +1,18 @@
 <script>
 export default {
- data() {
+  data() {
     return {
-      arr: ['a', 'b', 'c'],
+      cssClasses: "color: red",
     }
   },
   methods: {
-    add: function () {
-      this.arr.push('xxx');
-    },
-    delFirst: function(){
-        this.arr.shift();
-    },
-    delLast: function() {
-      this.arr.pop();
-    },
-    delSecondLast: function() {
-      this.arr.splice(-2,1);
-    },
-    sortarr: function(){
-      this.arr.sort();
-    },
-    reverseMassive: function(){
-      this.arr.reverse();
-    }
+    
   }
 
 }
 </script>
 <template>
-<p class="a">Я уже стилизирован</p>
-<p class="b"> А я тем более</p>
+  <p :style="cssClasses">text</p>
 </template>
 
 
