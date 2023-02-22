077e9e85351dd49f8bb96f291a05efb78fe3f499
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-22 15:39:01 +0300

    Условные гурппы в vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,25 +2,23 @@
 export default {
   data() {
     return {
-          age: 2,  
+         isAuth: true, 
     }
   },
   methods: {
     toggle: function () {
-      this.visible = !this.visible;
-    },
-    toggle1: function () {
-      this.visible1 = !this.visible1;
-    },
-    toggle2: function () {
-      this.visible2 = !this.visible2;
+      this.isAuth = !this.isAuth;
     }
     
   }
 }
 </script>
 <template>
-  <p v-if="age < 18"> Подросток</p>
-  <p v-else-if="age <= 25"> Молодой человек</p>
-  <p v-else-if="age >= 26"> Мужчина</p>
+   <button @click="toggle">Кнопка</button>
+   <br>
+  <div v-if="isAuth">
+    <p>+++</p>
+    <p>+++</p>
+    <p>+++</p>
+  </div>
 </template>
