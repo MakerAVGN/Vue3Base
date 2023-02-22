b17b22da8dde967310c3ad4dba42b398dac9f455
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-22 16:47:00 +0300

    Получение ключей при переборе массивов в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,28 +1,33 @@
 <style>
+*{
+  background-color: #3d405b;
+}
 div {
   display: flex;
   font-size: 1.2rem;
 }
 .a{
-  border: 3px solid purple;
+  border: 3px solid #e07a5f;
   margin: 5px;
-  padding: 5px;
+  padding: 10px;
   align-items: center;
+  flex-direction: column;
+  color:#e07a5f ;
 }
 .b{
-  border: 3px solid yellow;
+  border: 3px solid #f4f1de;
   flex-direction: column;
   text-align: center;
   margin: 5px;
+  padding: 10px;
+  color: #f4f1de;
 }
 </style>
 <script>
 export default {
   data() {
     return {
-      items: [1, 2, 3, 4, 5],
-      items1: [1, 2, 3, 4, 5],
-      items2: [1, 2, 3, 4, 5],
+        arr: ['x', 'y', 'z'],
     }
   },
   methods: {
@@ -34,19 +39,13 @@ export default {
 }
 </script>
 <template>
-  <p v-for="elem in items" class="b">{{ elem }}</p>
-  <br>
-  <br>
-  <div class="a">
-  <p v-for="amo in items1">{{ amo*amo }};</p>
-  </div>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-<p v-for="go in items2" class="b" ><ul><li>{{ go }}</li></ul></p>
+ <p class="b" v-for="(elem, key) in arr">
+  		 <ul><li>{{ key + 1 }} {{ elem }}</li></ul>
+  	</p>
 
+  <p class="a" v-for="(elem, key) in arr">
+    		 <ul><li>{{ key + 1 }} {{ elem }}</li></ul>
+    	</p>
 </template>
 
 
