37d1c35c73db30de380419a0ed9e0669285cab80
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-22 18:06:51 +0300

    Реактивность массивов в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,30 +2,43 @@
 export default {
  data() {
     return {
-      products: [
-        {
-          id: 1,
-          name: 'product1',
-        },
-        {
-          id: 2,
-          name: 'product2',
-        },
-        {
-          id: 3,
-          name: 'product3',
-        },
-      ]
+      arr: ['a', 'b', 'c'],
+    }
+  },
+  methods: {
+    add: function () {
+      this.arr.push('xxx');
+    },
+    delFirst: function(){
+        this.arr.shift();
+    },
+    delLast: function() {
+      this.arr.pop();
+    },
+    delSecondLast: function() {
+      this.arr.splice(-2,1);
+    },
+    sortarr: function(){
+      this.arr.sort();
+    },
+    reverseMassive: function(){
+      this.arr.reverse();
     }
   }
+
 }
 </script>
 <template>
-<ul>
-    <li v-for="user in products" :key="user.id">
-      {{ user.name }}
-    </li>
-  </ul>
+ <button class="a" @click="add" >Task1</button>
+  <button class="a"  @click="delFirst" >Task2</button>
+  <button class="a"  @click="delLast" >Task3</button>
+  <button class="a"  @click="delSecondLast" >Task4</button>
+  <button class="a"  @click="sortarr" >Task5</button>
+  <button class="a"  @click="reverseMassive" >Task6</button>
+
+  <ul class="b"  v-for="elem in arr">
+      <li>{{ elem }}</li>
+    </ul><br>
 </template>
 
 
