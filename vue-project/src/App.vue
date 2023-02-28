9f45672c69c192cd8a4948f6be28ef8598c45069
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-28 21:57:33 +0300

    Инлайн объект с CSS классами в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,35 +2,21 @@
 export default {
   data() {
     return {
-      obj: {
-        hidden: true,
-      },
     }
   },
   methods: {
-    setfalse: function () {
-      this.obj.hidden = false;
-    },
-    settrue: function () {
-      this.obj.hidden = true;
-    },
-    setDone: function () {
-      this.obj.hidden = !this.obj.hidden;
-  }
+    
 }
 }
 </script>
 <template>
-  <p :class="obj">text</p>
-  	<button @click="settrue">hide</button> <br>
-    <button @click="setfalse">show</button><br>
-      <button @click="setDone">{{ obj.hidden ? "show" : "hide" }}№4</button><br />
+  <p :class="{ active: true, valid: false }">
+    text
+  </p>
 </template>
 
 <style>
-p.hidden {
-	display: none;
-}
+
 </style>
 
 
