c0baf726dcf59dbc10c3191012178f85ae18b623
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-02-28 21:52:51 +0300

    Реактивность объекта с CSS классами в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,16 +3,34 @@ export default {
   data() {
     return {
       obj: {
-        done: true,
-        selected: false,
+        hidden: true,
       },
     }
+  },
+  methods: {
+    setfalse: function () {
+      this.obj.hidden = false;
+    },
+    settrue: function () {
+      this.obj.hidden = true;
+    },
+    setDone: function () {
+      this.obj.hidden = !this.obj.hidden;
   }
+}
 }
 </script>
 <template>
   <p :class="obj">text</p>
+  	<button @click="settrue">hide</button> <br>
+    <button @click="setfalse">show</button><br>
+      <button @click="setDone">{{ obj.hidden ? "show" : "hide" }}№4</button><br />
 </template>
 
+<style>
+p.hidden {
+	display: none;
+}
+</style>
 
 
