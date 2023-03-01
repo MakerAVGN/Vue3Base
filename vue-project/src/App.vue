210d13a7aace876be424ea8e5f0b2f616766c26a
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-01 22:57:05 +0300

    Получение данных формы по событию в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,23 +2,55 @@
 export default {
   data() {
     return {
-      task: "Task",
-      number: 3,
-    }
+      number: 1,
+      number_square: "Task 1",
+      number1: 0,
+      number2: 0,
+      sum_number: "Task 2",
+      text1: "placeholder",
+      text2: "placeholder",
+      fullName: "",
+      userName: "",
+      userSurname: "",
+      userPatronymic: "",
+    };
   },
   methods: {
-    
-}
-}
+    square: function () {
+      this.number_square = this.number ** 2;
+    },
+    sum: function () {
+      this.sum_number = this.number1 + this.number2;
+    },  
+    swap: function () {
+      const temp = this.text1;
+      this.text1 = this.text2;
+      this.text2 = temp;
+    },
+  },
+};
 </script>
 <template>
- <input type="text" v-model="task" /><br>
-  <p>{{ task }}</p><br>
-  <p>{{ task.toUpperCase() }}</p><br>
-  <input type="number" v-model="number" /><br>
-  <p>{{ number ** 2 }}</p><br>
+    <input type="number" v-model="number" />
+    <button @click="square">Square of number</button>
+    <br>
+    <p>{{ number_square }}</p>
+    <br>
+    
+    <input type="number" v-model="number1" />
+    <input type="number" v-model="number2" />
+    <button @click="sum">Summ of numbers</button>
+    <br>
+    
+    <p>{{ sum_number }}</p>
+    <br>
+    
+    <input type="text" v-model="text1" />
+    <input type="text" v-model="text2" />
+    <button @click="swap">Swap values</button>
+    <br>
+    
 </template>
-
 <style>
 
 </style>
