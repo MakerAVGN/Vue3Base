b373b1c3dda445e64910f951b616f83d836009c6
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-03-01 23:02:01 +0300

    Работа с textarea в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,54 +2,32 @@
 export default {
   data() {
     return {
-      number: 1,
-      number_square: "Task 1",
-      number1: 0,
-      number2: 0,
-      sum_number: "Task 2",
-      text1: "placeholder",
-      text2: "placeholder",
-      fullName: "",
-      userName: "",
-      userSurname: "",
-      userPatronymic: "",
+      text: "",
+      text_button: "",
+      text_array: "",
     };
   },
   methods: {
-    square: function () {
-      this.number_square = this.number ** 2;
-    },
-    sum: function () {
-      this.sum_number = this.number1 + this.number2;
-    },  
-    swap: function () {
-      const temp = this.text1;
-      this.text1 = this.text2;
-      this.text2 = temp;
+    split_text: function () {
+      this.text_array = this.text_button.split(" ");
     },
   },
 };
 </script>
 <template>
-    <input type="number" v-model="number" />
-    <button @click="square">Square of number</button>
-    <br>
-    <p>{{ number_square }}</p>
-    <br>
-    
-    <input type="number" v-model="number1" />
-    <input type="number" v-model="number2" />
-    <button @click="sum">Summ of numbers</button>
-    <br>
-    
-    <p>{{ sum_number }}</p>
-    <br>
-    
-    <input type="text" v-model="text1" />
-    <input type="text" v-model="text2" />
-    <button @click="swap">Swap values</button>
-    <br>
-    
+  <textarea v-model="text"></textarea>
+  &nbsp;
+  <p>{{ text }}</p>
+  &nbsp;
+  <textarea v-model="text_button"></textarea>
+  &nbsp;
+  <button @click="split_text">Array</button>
+  &nbsp;
+  <ul v-for="elem in text_array">
+    <li :key="key">
+      {{ elem }}
+    </li>
+  </ul>
 </template>
 <style>
 
