53e454ec72cb4ac206d5d2d8895ca348acec1ec8
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-05-18 22:25:14 +0300

    Формирование селекта в цикле в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,22 +1,42 @@
 <script>
 export default {
- data() {
+  data() {
     return {
-      selected: 'Симферополь', // значение по умолчанию
+      day: 'Понедельник',
+      days: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+      month: 'Январь',
+      months: ['Январь', 'Февраль'],
+      year: '2000',
+      years: ['2000', '2001']
     }
   },
   methods: {},
 };
 </script>
 <template>
-  <p>В каком городе вы живете?</p>
- 	<select v-model="selected">
-  		<option>Симферополь</option>
-  		<option>Севастополь</option>
-  		<option>Керчь</option>
+  <div>
+ <select v-model="day">
+  		<option v-for="day in days">{{ day }}</option>
   	</select>
-	
   	<p>{{ selected }}</p>
+  </div>
+
+  <div>
+    <select v-model="month">
+    		<option v-for="month in months">{{ month }}</option>
+    	</select>
+    	<p>{{ selected }}</p>
+  </div>
+
+  <div>
+      <select v-model="year">
+      		<option v-for="year in years">{{ year }}</option>
+      	</select>
+      	<p>{{ selected }}</p>
+    </div>
+
+
+
 
 
 
