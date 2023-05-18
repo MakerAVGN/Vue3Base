90f674bb4e3d276e5608a7c4e11a8e70b18d1a0f
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-05-18 23:21:31 +0300

    Блокировка элементов в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,38 +2,22 @@
 export default {
   data() {
     return {
-      day: 'Понедельник',
-      days: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-      month: 'Январь',
-      months: ['Январь', 'Февраль'],
-      year: '2000',
-      years: ['2000', '2001']
+      isDisabled: false,
     }
   },
-  methods: {},
+  methods: {
+    a: function () {
+      this.isDisabled = !this.isDisabled
+    }
+  },
 };
 </script>
 <template>
-  <div>
- <select v-model="day">
-  		<option v-for="day in days">{{ day }}</option>
-  	</select>
-  	<p>{{ selected }}</p>
-  </div>
 
-  <div>
-    <select v-model="month">
-    		<option v-for="month in months">{{ month }}</option>
-    	</select>
-    	<p>{{ selected }}</p>
-  </div>
 
-  <div>
-      <select v-model="year">
-      		<option v-for="year in years">{{ year }}</option>
-      	</select>
-      	<p>{{ selected }}</p>
-    </div>
+  	<input :disabled="isDisabled" >
+
+    <input type="checkbox" v-model="isDisabled">
 
 
 
