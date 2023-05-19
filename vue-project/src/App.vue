a631ec8dee19669871d1a60313299ce4e5b1f6a8
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-05-19 20:33:45 +0300

    Форма для редактирования массива объектов в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,49 +6,54 @@ export default {
         {
           id: 1,
           name: 'name1',
-          salary: 100,
-          age: 30,
+          surn: 'surn1',
+          isEdit: false,
         },
         {
           id: 2,
           name: 'name2',
-          salary: 200,
-          age: 40,
+          surn: 'surn2',
+          isEdit: false,
         },
         {
           id: 3,
           name: 'name3',
-          salary: 300,
-          age: 50,
+          surn: 'surn3',
+          isEdit: false,
         },
-      ],
+      ]
     }
   },
  methods: {
-    removeItem: function (id) {
-      this.users = this.users.filter((user) => {
-        return user.id !== id;
-      })
+    edit(user) {
+      user.isEdit = true;
+    },
+    save(user) {
+      user.isEdit = false;
     }
   },
 };
 </script>
 <template>
-
   <ul>
-  		<li v-for="user in users" :key="user.id">
-  			{{ user.name }}
-  			{{ user.salary }}
-        {{ user.age }}
-        <button @click="removeItem(user.id)">remove</button>
-  		</li>
-  	</ul>
-
-
-
-
-
-</template> 
+    <li v-for="user in users" :key="user.id">
+      <template v-if="!user.isEdit">
+        {{ user.name }}
+        {{ user.surn }}
+        <button @click="edit(user)">
+          edit
+        </button>
+      </template>
+      <template v-else>
+        <input v-model="user.name">
+        <input v-model="user.surn">
+        <button @click="save(user)">
+          save
+        </button>
+      </template>
+    </li>
+  </ul>
+</template>
 <style>
 
 </style>
