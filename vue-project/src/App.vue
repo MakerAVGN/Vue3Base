f91ca0b627b2b9065ae20fa4fbe3d8f6f854497a
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-05-20 16:31:17 +0300

    Блокнот

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,79 +1,92 @@
+<template>
+  <div>
+    <div class="sidebar">
+      <input class="search" type="text" v-model="searchText" placeholder="Search...">
+      <ul>
+        <li v-for="note in filteredNotes" :key="note.id" @click="selectNote(note)">
+          {{ note.title }}
+          <button @click.stop="deleteNote(note)">Delete</button>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <textarea v-model="currentNote.content"></textarea>
+      <button @click="editMode = !editMode">{{ editMode ? 'Save' : 'Edit' }}</button>
+    </div>
+  </div>
+</template>
+
 <script>
-import User from './components/User.vue'
-import Employee from './components/Employee.vue'
-
 export default {
- data() {
+  data() {
     return {
-      users: [
-        {
-          id: 1,
-          name: 'Учеба',
-          surn: 'Сделать домашку по аипу'
-        },
-        {
-          id: 2,
-          name: 'Готовка',
-          surn: 'Приготовить себе спагетти'
-        },
-        {
-          id: 3,
-          name: 'Уборка',
-          surn: 'Прибраться в своей комнате'
-        },
+      notes: [
+        { id: 1, title: 'Note 1', content: 'Content of note 1' },
+        { id: 2, title: 'Note 2', content: 'Content of note 2' },
+        { id: 3, title: 'Note 3', content: 'Content of note 3' },
       ],
-    }
+      currentNote: { id: null, title: '', content: '' },
+      editMode: false,
+      searchText: '',
+    };
   },
-  components: {
-      User,
-      Employee
+  computed: {
+    filteredNotes() {
+      return this.notes.filter((note) =>
+        note.title.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    },
   },
- methods: {
-  change(id, name, surn) {
-      this.users = this.users.map((user) => {
-        if (user.id === id) {
-          user.name = name;
-          user.surn = surn;
+  methods: {
+    selectNote(note) {
+      this.currentNote = { ...note };
+      this.editMode = false;
+    },
+    saveNote() {
+      const index = this.notes.findIndex((note) => note.id === this.currentNote.id);
+      if (index !== -1) {
+        this.notes[index] = { ...this.currentNote };
+      }
+    },
+    deleteNote(note) {
+      const index = this.notes.findIndex((n) => n.id === note.id);
+      if (index !== -1) {
+        this.notes.splice(index, 1);
+        this.currentNote = { id: null, title: '', content: '' };
+      }
+    },
+  },
+  watch: {
+    currentNote: {
+      handler() {
+        if (this.editMode) {
+          this.saveNote();
         }
-        return user;
-      });
-    },
-    remove(id) {
-      this.users = this.users.filter((user) => {
-        return user.id !== id;  
-      })
-    },
-    add(name, surn) {
-      let id = this.users.length + 1;
-
-      this.users.push({
-        id,
-        name,
-        surn
-      });
+      },
+      deep: true,
     },
   },
 };
 </script>
-<template>
 
- <User
-  		v-for   ="user in users"
-		
-  		:id     ="user.id"
-  		:name   ="user.name"
-  		:surn   ="user.surn"
-  		@remove ="remove"
-      @change="change"
-      @add="add"
-  		:key    ="user.id"
-  	/>
-
-</template>
 <style>
-ul.done {
-  text-decoration: line-through;
+.sidebar {
+  width: 200px;
+  float: left;
+  padding: 10px;
+}
+
+.content {
+  margin-left: 220px;
+  padding: 10px;
+}
+
+textarea {
+  width: 100%;
+  height: 300px;
+}
+
+.search{
+  height: 100px;
 }
 </style>
-
-
